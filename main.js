c1effc424dddbf32e3557f5fde4d1874be829b5b
Devarite <robloxnuexr@gmail.com>
2022-03-24 20:27:34 +1100

    eh

--- main.js
+++ main.js
@@ -3,21 +3,63 @@ $(document).ready(function(){
         $(this).addClass('pressed').siblings().removeClass('pressed');
     });
 
-    $('#projects').click(function(){
-        $('.projects').show();
+    $('#tools').click(function(){
+        $('.tools').show();
         $('.about').hide();
-        $('.art').hide();
     });
 
     $('#about').click(function(){
         $('.about').show();
-        $('.projects').hide();
-        $('.art').hide();
+        $('.tools').hide();
     });
 
-    $('#art').click(function(){
-        $('.art').show();
-        $('.projects').hide();
-        $('.about').hide();
+    //password generator
+    //takes in the length of the password
+    // takes in the true/false for numbers
+    // takes in the true/false for symbols
+    // takes in the true/false for uppercase
+    // takes in the true/false for lowercase
+    // takes in the true/false for special characters
+    // returns the password
+
+    generatePassword = function(length, numbers, symbols, uppercase, lowercase, special){
+        var generatedPassword = "";
+        var possible = "";
+        if(numbers){
+            possible += "0123456789";
+        }
+        if(symbols){
+            possible += "!@#$%^&*()";
+        }
+        if(uppercase){
+            possible += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if(lowercase){
+            possible += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if(special){
+            possible += "~`!@#$%^&*()_-+={[}]|:;'<,>.?/";
+        }
+        for(var i = 0; i < length; i++){
+            generatedPassword += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return generatedPassword;
+    }
+
+    $('#generate').click(function(){
+        var length = $('#length').val();
+        var numbers = $('#numbers').prop('checked');
+        var symbols = $('#symbols').prop('checked');
+        var uppercase = $('#uppercase').prop('checked');
+        var lowercase = $('#lowercase').prop('checked');
+        var special = $('#special').prop('checked');
+        var password = generatePassword(length, numbers, symbols, uppercase, lowercase, special);
+        $('#password').val(password);
     });
+
+    var password = $('#password');
+    //copy password to clipboard
+    password.select();
+
+    navigator.clipboard.writeText(password.val());
 });
