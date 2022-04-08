11ed0485ed8d968931beef9664b65cd155150a93
Devarite <robloxnuexr@gmail.com>
2022-04-09 06:00:05 +1000

    scroll to top button

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //set.pcp
 
-//header buttons
 $(document).ready(function(){
+    //header buttons
     $('.tab').click(function(){
         $(this).addClass('pressed').siblings().removeClass('pressed');
     });
@@ -14,5 +14,19 @@ $(document).ready(function(){
     $('#about').click(function(){
         $('.about').show();
         $('.projects').hide();
+    });
+
+    //scroll to top button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 20) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0},200);
+        return false;
     });
 });
